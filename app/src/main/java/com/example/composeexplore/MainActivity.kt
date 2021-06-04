12a4aa9b89b506e2215651f0c01a2bfaa1938306
telkomdev-odiapratama <odiapratama@gmail.com>
2021-06-04 11:16:29 +0700

    Add styling

--- app/src/main/java/com/example/composeexplore/MainActivity.kt
+++ app/src/main/java/com/example/composeexplore/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.composeexplore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeexplore.ui.theme.ComposeExploreTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,10 +25,20 @@ class MainActivity : ComponentActivity() {
             ComposeExploreTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    InitUI()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun InitUI() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Greeting("Android")
+        NewStory()
     }
 }
 
@@ -29,10 +47,41 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun NewStory() {
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Bandung",
+                style = typography.h6
+            )
+            Text(
+                text = "West Java",
+                style = typography.body2
+            )
+            Text(
+                text = "Indonesia",
+                style = typography.body2
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeExploreTheme {
-        Greeting("Android")
+        InitUI()
     }
 }
